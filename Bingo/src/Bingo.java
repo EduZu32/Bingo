6918d6614181831e344d33d5e4400eceaace2f63
EduZu32 <apa.eduard@gmail.com>
2023-11-30 00:31:26 +0100

    Carton
    Cartón de 10 números aleatorios de 1 al 100.

--- Bingo/src/Bingo.java
+++ Bingo/src/Bingo.java
@@ -1,12 +1,41 @@
-public class Bingo {
-    public static void main(String[] args) {
+import java.util.Random;
 
-        int[] numeros = new int[10];
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int) (Math.random()*99) +1;
+public class Bingo {
+
+    public static void main(String[] args) {
+        
+
+        //carton con 10 numeros
+        int[] carton = new int[10];
+        int i = 0, n;
+        Random r = new Random();
+
+        // numeros alatorios del carton de 1 al 100
+        while (i < 10) {
+            n = r.nextInt(100) + 1;
+            if(! Busqueda(carton,n,i)){
+                carton[i] =n;
+                i++;
+            }
 
         }
+        for (int j = 0; j < carton.length;j++) {
+            System.out.print(carton[j]+" ");
+        }
+
 
     }
+
+    public static boolean Busqueda(int [] carton,int n, int i){
+        int j;
+        for (j=0;j<i;j++){
+            if (n == carton[j]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
